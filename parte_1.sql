918fc21f2d5e4f1ca4c099fb6b870abc5f3a4a96
wallacesfos <wallace.oliveira@aluno.faculdadeimpacta.com.br>
2022-01-25 20:18:59 -0300

    França arrumado

--- parte_1.sql
+++ parte_1.sql
@@ -7,7 +7,7 @@ create table kopas(
 );
 
 
-insert into kopas(selecao, qnt_copas) values ('Brasil',5), ('Alemanha',5), ('Itália',4) ,('Argentina',2),('rança',2),('Uruguai',2),('Inglaterra',1),('Espanha',1),('Japão',1);
+insert into kopas(selecao, qnt_copas) values ('Brasil',5), ('Alemanha',5), ('Itália',4) ,('Argentina',2),('frança',2),('Uruguai',2),('Inglaterra',1),('Espanha',1),('Japão',1);
 
 select selecao, qnt_copas from kopas;
 
