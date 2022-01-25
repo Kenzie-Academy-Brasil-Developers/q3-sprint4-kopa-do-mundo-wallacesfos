04947c5e9306ce1a0d14a00b8d0efe8bc9bfa794
wallacesfos <wallace.oliveira@aluno.faculdadeimpacta.com.br>
2022-01-25 15:58:39 -0300

    Feat: Terminadoi

---
+++ parte_1.sql
@@ -0,0 +1,14 @@
+create database kopa_db;
+
+create table kopas(
+	id 	BIGSERIAL CONSTRAINT pk_kopas primary key,
+	selecao VARCHAR(128) unique,
+	qnt_copas INTEGER
+);
+
+
+insert into kopas(selecao, qnt_copas) values ('Brasil',5), ('Alemanha',5), ('Itália',4) ,('Argentina',2),('rança',2),('Uruguai',2),('Inglaterra',1),('Espanha',1),('Japão',1)
+
+select selecao, qnt_copas from kopas
+
+update kopas set qnt_copas = 4 where selecao = 'Alemanha'
